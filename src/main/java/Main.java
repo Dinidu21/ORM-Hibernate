b0c69613d054e26f7044a6e0c2d578b854813475
Dinidu21 <dinidusachintha3@gmail.com>
2025-03-04 23:55:35 +0530

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 import entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
-        Configuration config = new Configuration().configure();
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(entity.Student.class);
+        config.configure();
 
         Student student = new Student();
         student.setId(1);
@@ -13,11 +16,12 @@ public class Main {
         student.setAge(23);
 
         Session session;
-        try (SessionFactory factory = config.buildSessionFactory()) {
-            session = factory.openSession();
-        }
+        SessionFactory factory = config.buildSessionFactory();
+        session = factory.openSession();
+
+
+        Transaction tx = session.beginTransaction();
         session.persist(student);
-
-
+        tx.commit();
     }
 }
--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -1,5 +1,7 @@
 package entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Getter
@@ -7,8 +9,10 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor
+@Entity
 
 public class Student {
+    @Id
     private int id;
     private String name;
     private int age;
